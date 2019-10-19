8443f66451be88711edb854646a5ba933396ba0d
Michael Johnson <michael.a.johnson.wa@gmail.com>
2019-10-19 11:02:07 -0700

    getting up and running tutorial completed

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'brand-blue': '#1992d4',
+      },
+      spacing: {
+        '72': '18rem',
+      }
+    }
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
   plugins: []
 }
